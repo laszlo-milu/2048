d7b9d0af8f372856123845aba7738c0e2e91672e
Milu László <milu_laci@milu.laci-HP-ProBook-6560b>
2016-05-18 15:35:35 +0200

    added add_new_number function which fills a zero slot with a 2 when 'space' is pressed

--- 2048.py
+++ 2048.py
@@ -6,9 +6,21 @@ win = curses.newwin(curses.LINES, curses.COLS, 0, 0)
 win.keypad(1)
 win.border(0)
 win.nodelay(1)
+import random
+
+def add_new_number():
+    global numbers
+    y=random.randrange(4)
+    x=random.randrange(4)
+    if (numbers[y][x] == 0):
+        numbers[y][x] = 2
+    else:
+        add_new_number()
 
 #numbers = [[1,2,3,4],[5,6,7,8],[1,22,333,4444],[1111,2222,3333,4444]]
 numbers = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+
 while True:
     y = 1
     x = 1
@@ -17,27 +29,9 @@ while True:
         for x in range(4):
             win.addstr(1+y*2,1+x*5,str(numbers[y][x]))
 
-    '''win.addstr(y,x,str(numbers[0][0]))
-    win.addstr(y,x+5,str(numbers[0][1]))
-    win.addstr(y,x+10,str(numbers[0][2]))
-    win.addstr(y,x+15,str(numbers[0][3]))
-
-    win.addstr(y+2,x,str(numbers[1][0]))
-    win.addstr(y+2,x+5,str(numbers[1][1]))
-    win.addstr(y+2,x+10,str(numbers[1][2]))
-    win.addstr(y+2,x+15,str(numbers[1][3]))
-
-    win.addstr(y+4,x,str(numbers[2][0]))
-    win.addstr(y+4,x+5,str(numbers[2][1]))
-    win.addstr(y+4,x+10,str(numbers[2][2]))
-    win.addstr(y+4,x+15,str(numbers[2][3]))
-
-    win.addstr(y+6,x,str(numbers[3][0]))
-    win.addstr(y+6,x+5,str(numbers[3][1]))
-    win.addstr(y+6,x+10,str(numbers[3][2]))
-    win.addstr(y+6,x+15,str(numbers[3][3]))'''
-
-    win.refresh()
+    # win.refresh()
+    if ch == 32:
+        add_new_number()
     if ch == 27:
         break
 
