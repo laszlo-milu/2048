3be51bbd453823a27299bdae69883cb24bd9a0da
Milu László <milu_laci@milu.laci-HP-ProBook-6560b>
2016-05-19 15:01:55 +0200

    movements added to the game

--- 2048.py
+++ 2048.py
@@ -9,13 +9,84 @@ win.border(0)
 win.nodelay(1)
 import random
 
+def bubble_sort_left(x):
+    global numbers
+    exchanges = True
+    passnum = len(numbers)-1
+    while passnum > 0 and exchanges:
+       exchanges = False
+       for i in range(passnum):
+           if numbers[x][i]<numbers[x][i+1] and numbers[x][i]==0:
+               exchanges = True
+               temp = numbers[x][i]
+               numbers[x][i] = numbers[x][i+1]
+               numbers[x][i+1] = temp
+       passnum = passnum-1
+
+def bubble_sort_up(y):
+    global numbers
+    exchanges = True
+    passnum = len(numbers)-1
+    while passnum > 0 and exchanges:
+       exchanges = False
+       for i in range(passnum):
+           if numbers[i][y]<numbers[i+1][y] and numbers[i][y]==0:
+               exchanges = True
+               temp = numbers[i][y]
+               numbers[i][y] = numbers[i+1][y]
+               numbers[i+1][y] = temp
+       passnum = passnum-1
+
+def bubble_sort_right(x):
+    global numbers
+    exchanges = True
+    passnum = len(numbers)-1
+    while passnum > 0 and exchanges:
+       exchanges = False
+       for i in range(passnum):
+           if numbers[x][i]>numbers[x][i+1] and numbers[x][i+1]==0:
+               exchanges = True
+               temp = numbers[x][i]
+               numbers[x][i] = numbers[x][i+1]
+               numbers[x][i+1] = temp
+       passnum = passnum-1
+
+def bubble_sort_down(y):
+    global numbers
+    exchanges = True
+    passnum = len(numbers)-1
+    while passnum > 0 and exchanges:
+       exchanges = False
+       for i in range(passnum):
+           if numbers[i][y]>numbers[i+1][y] and numbers[i+1][y]==0:
+               exchanges = True
+               temp = numbers[i][y]
+               numbers[i][y] = numbers[i+1][y]
+               numbers[i+1][y] = temp
+       passnum = passnum-1
+
+# def add_left(x):
+#     global numbers
+#
+
+
 def key_left_pressed():
+
+    for x in range(4):
+        bubble_sort_left(x)
 
 def key_right_pressed():
 
+    for x in range(4):
+        bubble_sort_right(x)
+
 def key_up_pressed():
+    for y in range(4):
+        bubble_sort_up(y)
 
 def key_down_pressed():
+    for y in range(4):
+        bubble_sort_down(y)
 
 def add_new_number():
     global numbers
@@ -39,16 +110,16 @@ while True:
             win.addstr(1+y*2,1+x*5,str(numbers[y][x]))
 
     if ch == curses.KEY_LEFT:
-        key_left_pressed():
+        key_left_pressed()
         add_new_number()
     if ch == curses.KEY_RIGHT:
-        key_right_pressed():
+        key_right_pressed()
         add_new_number()
     if ch == curses.KEY_UP:
-        key_up_pressed():
+        key_up_pressed()
         add_new_number()
     if ch == curses.KEY_DOWN:
-        key_down_pressed():
+        key_down_pressed()
         add_new_number()
 
     if ch == 27:
