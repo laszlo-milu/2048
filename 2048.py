67dd1dd2a40e967bf7c71fec60cf51c98d7b99dd
Milu László <milu_laci@milu.laci-HP-ProBook-6560b>
2016-05-20 09:11:09 +0200

    game is done except game over and 2048 has to be handled

--- 2048.py
+++ 2048.py
@@ -9,8 +9,12 @@ win.border(0)
 win.nodelay(1)
 import random
 
+valid_move=1
+highest=0
+
 def bubble_sort_left(x):
     global numbers
+    global valid_move
     exchanges = True
     passnum = len(numbers)-1
     while passnum > 0 and exchanges:
@@ -21,10 +25,15 @@ def bubble_sort_left(x):
                 temp = numbers[x][i]
                 numbers[x][i] = numbers[x][i+1]
                 numbers[x][i+1] = temp
+                valid_move=1
+            # else:
+            #     if valid_move!=1:
+            #         valid_move=2
         passnum = passnum-1
 
 def bubble_sort_up(y):
     global numbers
+    global valid_move
     exchanges = True
     passnum = len(numbers)-1
     while passnum > 0 and exchanges:
@@ -35,10 +44,15 @@ def bubble_sort_up(y):
                 temp = numbers[i][y]
                 numbers[i][y] = numbers[i+1][y]
                 numbers[i+1][y] = temp
+                valid_move=1
+            # else:
+            #     if valid_move!=1:
+            #         valid_move=2
         passnum = passnum-1
 
 def bubble_sort_right(x):
     global numbers
+    global valid_move
     exchanges = True
     passnum = len(numbers)-1
     while passnum > 0 and exchanges:
@@ -49,10 +63,15 @@ def bubble_sort_right(x):
                 temp = numbers[x][i]
                 numbers[x][i] = numbers[x][i+1]
                 numbers[x][i+1] = temp
+                valid_move=1
+            # else:
+            #     if valid_move!=1:
+            #         valid_move=2
         passnum = passnum-1
 
 def bubble_sort_down(y):
     global numbers
+    global valid_move
     exchanges = True
     passnum = len(numbers)-1
     while passnum > 0 and exchanges:
@@ -63,94 +82,129 @@ def bubble_sort_down(y):
                 temp = numbers[i][y]
                 numbers[i][y] = numbers[i+1][y]
                 numbers[i+1][y] = temp
+                valid_move=1
+            # else:
+            #     if valid_move!=1:
+            #         valid_move=2
         passnum = passnum-1
 
 def add_left(x):
     global numbers
+    global valid_move
     for i in range(3):
         if numbers[x][i]==numbers[x][i+1] and numbers[x][i]!=0:
             numbers[x][i+1]=numbers[x][i+1]*2
             numbers[x][i]=0
+            valid_move=1
+        # else:
+        #     if valid_move!=1:
+        #         valid_move=2
 
 def add_up(y):
     global numbers
+    global valid_move
     for i in range(3):
         if numbers[i][y]==numbers[i+1][y] and numbers[i][y]!=0:
             numbers[i+1][y]=numbers[i+1][y]*2
             numbers[i][y]=0
+            valid_move=1
+        # else:
+        #     if valid_move!=1:
+        #         valid_move=2
 
 def add_right(x):
     global numbers
+    global valid_move
     for i in range(3):
         if numbers[x][i]==numbers[x][i+1] and numbers[x][i+1]!=0:
             numbers[x][i]=numbers[x][i]*2
             numbers[x][i+1]=0
+            valid_move=1
+        # else:
+        #     if valid_move!=1:
+        #         valid_move=2
 
 def add_down(y):
     global numbers
+    global valid_move
     for i in range(3):
         if numbers[i][y]==numbers[i+1][y] and numbers[i][y]!=0:
             numbers[i][y]=numbers[i][y]*2
             numbers[i+1][y]=0
+            valid_move=1
+        # else:
+        #     if valid_move!=1:
+        #         valid_move=2
 
 
 def key_left_pressed():
+    global valid_move
     for x in range(4):
         bubble_sort_left(x)
         add_left(x)
         bubble_sort_left(x)
-    try:
-        add_new_number()
-    except:
-        curses.endwin()
-        exit()
+    add_new_number()
 
 def key_right_pressed():
+    global valid_move
     for x in range(4):
         bubble_sort_right(x)
         add_right(x)
         bubble_sort_right(x)
-    try:
-        add_new_number()
-    except:
-        curses.endwin()
-        exit()
+    add_new_number()
 
 def key_up_pressed():
+    global valid_move
     for y in range(4):
         bubble_sort_up(y)
         add_up(y)
         bubble_sort_up(y)
-    try:
-        add_new_number()
-    except:
-        curses.endwin()
-        exit()
+    add_new_number()
 
 def key_down_pressed():
+    global valid_move
     for y in range(4):
         bubble_sort_down(y)
         add_down(y)
         bubble_sort_down(y)
-    try:
-        add_new_number()
-    except:
-        curses.endwin()
-        exit()
+    add_new_number()
+
 
 def add_new_number():
     global numbers
+    global valid_move
+    global highest
     y=random.randrange(4)
     x=random.randrange(4)
-    if (numbers[y][x] == 0):
+    # numbers[y][x] == 0 and
+    if numbers[y][x] == 0 and valid_move==1:
         numbers[y][x] = 2
-    else:
+        valid_move=0
+        highest=max(numbers)
+
+    elif valid_move==1:
         add_new_number()
+        win.addstr(10,5,"                   ")
+    else:
+        valid_move=2
+        if valid_move==2:
+            win.addstr(10,5,"Try other direction")
+
+
+    # elif numbers[y][x] != 0 and valid_move==2:
+    #     # valid_move=0
+    #     add_new_number()
+    # else:
+    #     valid_move=0
+
+        # else:
+        # curses.endwin()
+        # exit()
 
 # numbers = [[1,2,3,4],[5,6,7,8],[1,22,333,4444],[1111,2222,3333,4444]]
-numbers = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+numbers = [[0,0,0,1024],[0,0,0,1024],[0,0,0,0],[0,0,0,0]]
 
-
+add_new_number()
 while True:
     # y = 1
     # x = 1
@@ -159,6 +213,9 @@ while True:
         for x in range(4):
             win.addstr(1+y*2,1+x*5,"    ")
             win.addstr(1+y*2,1+x*5,str(numbers[y][x]))
+            if highest==2048:
+                win.addstr(10,5,"2048!")
+                valid_move=0
 
     if ch == curses.KEY_LEFT:
         key_left_pressed()
@@ -172,6 +229,8 @@ while True:
     if ch == curses.KEY_DOWN:
         key_down_pressed()
 
+    # for y in range(4):
+    #     for x in range(4):
 
     if ch == 27:
         break
